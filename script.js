8a420a88228d75a23f6378522fcef622a0d93f83
Kreoniz <kreoniz@mail.ru>
2022-12-27 19:52:41 +0600

    Add changing colors functionality

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
+let dyeColor = "F9FFFE";
 const wool = document.querySelectorAll('.wool');
-console.log(wool);
-wool.forEach(elem => {
-    elem.style.backgroundColor = "#FFC0CB";
-});
+
+function dyeSheep(color) {
+    wool.forEach(elem => {
+        elem.style.backgroundColor = color;
+    });
+}
+dyeSheep(dyeColor);
+
+let rotateY = -40;
+let rotateX = 0;
+let rotateZ = 0;
+
+document.querySelector('.body').style.transform =
+`rotateY(${rotateY}deg) rotateX(${rotateX}deg) rotateZ(${rotateZ}deg)`;
+
+function handleChoice(event) {
+    const element = event.currentTarget;
+    document.querySelector('.active').classList.remove('active');
+    element.classList.add('active');
+    dyeColor = element.dataset.color;
+    dyeSheep(dyeColor);
+}
+
+const choices = document.querySelectorAll('.choice');
